6283481e237da7dac07f19efa3f72b6252f706c5
jeremy302 <olajeremy123@gmail.com>
2021-08-26 00:46:47 +0100

    patch

--- strings-4.c
+++ strings-4.c
@@ -27,8 +27,8 @@ char *str_append(char *dst, char *src)
  */
 char str_eq(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0' && (*s1++ == *s2++))
-		continue;
+	while (*s1 != '\0' && *s2 != '\0' && (*s1 == *s2))
+		s1++, s2++;
 	return (*s1 == '\0' && *s2 == '\0');
 }
 
